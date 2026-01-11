4d2c38302fc99cc36bc9db51dadd7f0c09841b50
Mirnes Nuhanović <mirnesnuhanovic9@gmail.com>
2026-01-11 14:40:08 +0100

    deleted checkmodels.py

--- backend/checkmodels.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyC15RUFxLJ6LCB7fdu8J-iTriKu4iuZVqA")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
